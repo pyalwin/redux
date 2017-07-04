3c6daf2323a2e735c263eff0412ebef079286b3a
pyalwin <arun.tgode@gmail.com>
2017-07-05 00:28:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from setup import basedir
 class BaseConfig(object):
     SECRET_KEY = "SO_SECURE"
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://omtkmoplveazgq:d8fad052a057018fd3d1493bad5054e2bd1c74c56d191d757e1fa096836175f7@ec2-54-221-220-82.compute-1.amazonaws.com:5432/d4su07rtkv112h'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
