f50bfeae29a421a9bf69efc6ebbd884ef9895de0
pyalwin <arun.tgode@gmail.com>
2017-07-05 10:17:45 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,4 +6,4 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+#bcrypt = Bcrypt(app)
